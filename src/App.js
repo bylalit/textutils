b7ff5cb8a74f3cb68f43e4be0feaeab2514a1d74
Lalit0101 <lalitkp0101@gmail.com>
2024-06-23 21:59:48 +0530

    done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      {/* <Navbar title="LalitKP" about="About Us"/> */}
+      <Navbar title="LalitKP" about="About Us"/>
       {/* <Navbar /> */}
     </>
   );
